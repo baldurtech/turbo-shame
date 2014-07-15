5d2ecb990e7f5ce0ec1a558c41efe7563f38314b
Chai Feng <chaifeng@chaifeng.com>
2014-07-15 14:25:39 +0800

    添加了最基本的连接数据库的代码

--- src/main/java/com/baldurtech/turbo/shame/ContactServlet.java
+++ src/main/java/com/baldurtech/turbo/shame/ContactServlet.java
@@ -6,9 +6,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ContactServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
         response.getWriter().println("Get contact by id: " + request.getParameter("contactId"));
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            // handle the error
+        }
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/baldurcontacts?user=baldurtech&password=baldurtechpwd");
+            response.getWriter().println(connection);
+            connection.close();
+        } catch(SQLException sqle) {
+            response.getWriter().println("Cannot connect to DB.");
+            response.getWriter().println(sqle.getMessage());
+            sqle.printStackTrace();
+        }
     }
 }
