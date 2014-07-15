7371d748973a7da7de7f723f7ad08fff0fb4c295
Chai Feng <chaifeng@chaifeng.com>
2014-07-15 18:02:46 +0800

    把查询到的 Contact 放到 Map 里面

--- src/main/java/com/baldurtech/turbo/shame/ContactServlet.java
+++ src/main/java/com/baldurtech/turbo/shame/ContactServlet.java
@@ -22,63 +22,8 @@ public class ContactServlet extends HttpServlet {
         throws IOException, ServletException {
         if(request.getParameter("contactId") == null) {
             response.getWriter().println("Get all contacts.");
-            List contacts = new ArrayList();
 
-            try {
-                Class.forName("com.mysql.jdbc.Driver").newInstance();
-            } catch (Exception ex) {
-                // handle the error
-            }
-
-            Connection connection = null;
-            Statement stmt = null;
-            ResultSet rs = null;
-            try {
-                connection = DriverManager.getConnection("jdbc:mysql://localhost/baldurcontacts?user=baldurtech&password=baldurtechpwd");
-                stmt = connection.createStatement();
-                rs = stmt.executeQuery("select * from contact");
-                while(rs.next()) {
-                    Map contact = new HashMap();
-
-                    contact.put("id", rs.getLong("id"));
-                    contact.put("name", rs.getString("name"));
-                    contact.put("mobile", rs.getString("mobile"));
-                    contact.put("vpmn", rs.getString("vpmn"));
-                    contact.put("job", rs.getString("job"));
-
-                    contacts.add(contact);
-                }
-            } catch(SQLException sqle) {
-                response.getWriter().println("Cannot connect to DB.");
-                response.getWriter().println(sqle.getMessage());
-                sqle.printStackTrace();
-            }
-
-            if(rs != null) {
-                try {
-                    rs.close();
-                } catch(Exception e) {
-
-                }
-            }
-
-            if(stmt != null) {
-                try {
-                    stmt.close();
-                } catch(Exception e) {
-
-                }
-            }
-
-            if(connection != null) {
-                try {
-                    connection.close();
-                } catch(Exception e) {
-
-                }
-            }
-
-            for(Object obj: contacts) {
+            for(Object obj: getAllContacts()) {
                 Map contact = (Map) obj;
 
                 response.getWriter().println("Id: " + contact.get("id"));
@@ -90,16 +35,7 @@ public class ContactServlet extends HttpServlet {
         } else {
             response.getWriter().println("Get contact by id: " + request.getParameter("contactId"));
 
-            Long id = null;
-            String name = null;
-            String mobile = null;
-            String vpmn = null;
-            String email = null;
-            String homeAddress = null;
-            String officeAddress = null;
-            String memo = null;
-            String job = null;
-            String jobLevel = null;
+            Map contact = new HashMap();
 
             try {
                 Class.forName("com.mysql.jdbc.Driver").newInstance();
@@ -115,16 +51,16 @@ public class ContactServlet extends HttpServlet {
                 stmt = connection.createStatement();
                 rs = stmt.executeQuery("select * from contact where id=" + request.getParameter("contactId"));
                 if(rs.next()) {
-                    id = rs.getLong("id");
-                    name = rs.getString("name");
-                    mobile = rs.getString("mobile");
-                    vpmn = rs.getString("vpmn");
-                    email = rs.getString("email");
-                    homeAddress = rs.getString("home_address");
-                    officeAddress = rs.getString("office_address");
-                    memo = rs.getString("memo");
-                    job = rs.getString("job");
-                    jobLevel = rs.getString("job_level");
+                    contact.put("id", rs.getLong("id"));
+                    contact.put("name", rs.getString("name"));
+                    contact.put("mobile", rs.getString("mobile"));
+                    contact.put("vpmn", rs.getString("vpmn"));
+                    contact.put("email", rs.getString("email"));
+                    contact.put("homeAddress", rs.getString("home_address"));
+                    contact.put("officeAddress", rs.getString("office_address"));
+                    contact.put("memo", rs.getString("memo"));
+                    contact.put("job", rs.getString("job"));
+                    contact.put("jobLevel", rs.getString("job_level"));
                 }
             } catch(SQLException sqle) {
                 response.getWriter().println("Cannot connect to DB.");
@@ -157,19 +93,76 @@ public class ContactServlet extends HttpServlet {
                 }
             }
 
-            if(id != null) {
-                response.getWriter().println("Name: " + name);
-                response.getWriter().println("Mobile: " + mobile);
-                response.getWriter().println("Vpmn: " + vpmn);
-                response.getWriter().println("Email: " + email);
-                response.getWriter().println("HomeAddress: " + homeAddress);
-                response.getWriter().println("OfficeAddress: " + officeAddress);
-                response.getWriter().println("Memo: " + memo);
-                response.getWriter().println("Job: " + job);
-                response.getWriter().println("JobLevel: " + jobLevel);
+            if(contact.get("id") != null) {
+                response.getWriter().println("Name: " + contact.get("name"));
+                response.getWriter().println("Mobile: " + contact.get("mobile"));
+                response.getWriter().println("Vpmn: " + contact.get("vpmn"));
+                response.getWriter().println("Email: " + contact.get("email"));
+                response.getWriter().println("HomeAddress: " + contact.get("homeAddress"));
+                response.getWriter().println("OfficeAddress: " + contact.get("officeAddress"));
+                response.getWriter().println("Memo: " + contact.get("memo"));
+                response.getWriter().println("Job: " + contact.get("job"));
+                response.getWriter().println("JobLevel: " + contact.get("jobLevel"));
             } else {
                 response.getWriter().println("Contact not found!");
             }
         }
+    }
+
+    private List getAllContacts() {
+        List contacts = new ArrayList();
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            // handle the error
+        }
+
+        Connection connection = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/baldurcontacts?user=baldurtech&password=baldurtechpwd");
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery("select * from contact");
+            while(rs.next()) {
+                Map contact = new HashMap();
+
+                contact.put("id", rs.getLong("id"));
+                contact.put("name", rs.getString("name"));
+                contact.put("mobile", rs.getString("mobile"));
+                contact.put("vpmn", rs.getString("vpmn"));
+                contact.put("job", rs.getString("job"));
+
+                contacts.add(contact);
+            }
+        } catch(SQLException sqle) {
+            sqle.printStackTrace();
+        }
+
+        if(rs != null) {
+            try {
+                rs.close();
+            } catch(Exception e) {
+
+            }
+        }
+
+        if(stmt != null) {
+            try {
+                stmt.close();
+            } catch(Exception e) {
+
+            }
+        }
+
+        if(connection != null) {
+            try {
+                connection.close();
+            } catch(Exception e) {
+
+            }
+        }
+        return contacts;
     }
 }
