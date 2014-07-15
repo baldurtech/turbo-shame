0a03cab0b7233f35cbd40a2d0d11f1f147b14e68
Chai Feng <chaifeng@chaifeng.com>
2014-07-15 14:46:17 +0800

    添加了最基本的查询，并且释放所有资源

--- src/main/java/com/baldurtech/turbo/shame/ContactServlet.java
+++ src/main/java/com/baldurtech/turbo/shame/ContactServlet.java
@@ -8,7 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ContactServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -21,14 +23,44 @@ public class ContactServlet extends HttpServlet {
             // handle the error
         }
 
+        Connection connection = null;
+        Statement stmt = null;
+        ResultSet rs = null;
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/baldurcontacts?user=baldurtech&password=baldurtechpwd");
-            response.getWriter().println(connection);
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/baldurcontacts?user=baldurtech&password=baldurtechpwd");
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery("select * from contact where id=1");
+            rs.next();
+            response.getWriter().println(rs.getString("name"));
             connection.close();
         } catch(SQLException sqle) {
             response.getWriter().println("Cannot connect to DB.");
             response.getWriter().println(sqle.getMessage());
             sqle.printStackTrace();
+        }
+
+        if(rs != null) {
+            try {
+                rs.close();
+            } catch(Exception e) {
+
+            }
+        }
+
+        if(stmt != null) {
+            try {
+                stmt.close();
+            } catch(Exception e) {
+
+            }
+        }
+
+        if(connection != null) {
+            try {
+                connection.close();
+            } catch(Exception e) {
+
+            }
         }
     }
 }
