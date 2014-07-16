f20b198d6b08eafafa5268a76e43206797b2f564
baldur <master@baldurtech.com>
2014-07-16 14:16:39 +0800

    重构 getContactById，抽取给 Contact 赋值的代码

--- src/main/java/com/baldurtech/turbo/shame/ContactServlet.java
+++ src/main/java/com/baldurtech/turbo/shame/ContactServlet.java
@@ -125,16 +125,7 @@ public class ContactServlet extends HttpServlet {
             stmt = connection.createStatement();
             rs = stmt.executeQuery("select * from contact where id=" + id);
             if(rs.next()) {
-                contact.setId(rs.getLong("id"));
-                contact.setName(rs.getString("name"));
-                contact.setMobile(rs.getString("mobile"));
-                contact.setVpmn(rs.getString("vpmn"));
-                contact.setEmail(rs.getString("email"));
-                contact.setHomeAddress(rs.getString("home_address"));
-                contact.setOfficeAddress(rs.getString("office_address"));
-                contact.setMemo(rs.getString("memo"));
-                contact.setJob(rs.getString("job"));
-                contact.setJobLevel(rs.getInt("job_level"));
+                contact = getContactFromResultSet(rs);
             }
         } catch(SQLException sqle) {
             sqle.printStackTrace();
@@ -164,6 +155,25 @@ public class ContactServlet extends HttpServlet {
 
             }
         }
+
+        return contact;
+    }
+
+    public Contact getContactFromResultSet(ResultSet rs)
+        throws SQLException {
+
+        Contact contact = new Contact();
+
+        contact.setId(rs.getLong("id"));
+        contact.setName(rs.getString("name"));
+        contact.setMobile(rs.getString("mobile"));
+        contact.setVpmn(rs.getString("vpmn"));
+        contact.setEmail(rs.getString("email"));
+        contact.setHomeAddress(rs.getString("home_address"));
+        contact.setOfficeAddress(rs.getString("office_address"));
+        contact.setMemo(rs.getString("memo"));
+        contact.setJob(rs.getString("job"));
+        contact.setJobLevel(rs.getInt("job_level"));
 
         return contact;
     }
