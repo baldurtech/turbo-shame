73fff5b1a656f8ef775cb310a90a738ea096a175
baldur <master@baldurtech.com>
2014-07-17 16:56:01 +0800

    访问 /contact 会重定向到 /contact/list

--- src/main/java/com/baldurtech/turbo/shame/ContactServlet.java
+++ src/main/java/com/baldurtech/turbo/shame/ContactServlet.java
@@ -10,29 +10,7 @@ public class ContactServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
 
-        ContactService contactService = new ContactService();
-
-        if(request.getParameter("contactId") == null) {
-
-            request.setAttribute("contactList", contactService.getAllContacts());
-
-            getServletContext()
-                .getRequestDispatcher("/WEB-INF/jsp/contact/list.jsp")
-                .forward(request, response);
-
-        } else {
-            Contact contact = contactService.getContactById(request.getParameter("contactId"));
-
-            if(contact != null) {
-                request.setAttribute("contact", contact);
-
-                getServletContext()
-                    .getRequestDispatcher("/WEB-INF/jsp/contact/show.jsp")
-                    .forward(request, response);
-            } else {
-                response.getWriter().println("Contact not found!");
-            }
-        }
+        response.sendRedirect("contact/list");
     }
 
 }
